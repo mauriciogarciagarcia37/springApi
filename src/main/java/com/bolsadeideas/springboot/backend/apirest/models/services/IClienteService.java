48900d4142ef86485e12e47f3a4470db7485e8de
Mauricio Garcia Garcia <mauriciogarciagarcia37@gmail.com>
2023-01-18 21:53:29 -0600

    SegundoCommit

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -0,0 +1,19 @@
+package com.bolsadeideas.springboot.backend.apirest.models.services;
+
+import java.util.List;
+
+import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
+
+public interface IClienteService {
+	public List<Cliente> findAll();
+	
+	public Cliente save(Cliente cliente);
+	
+	public Cliente findById(Long id);
+	
+	public void delete( Long id);
+	
+	
+	
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+/* Populate tabla clientes */
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Andres', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Adrian', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Diego', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Lilia', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Adriana', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mauricio', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Cloud', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Alejandro', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Mario', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Luis', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Antonio', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
+INSERT INTO clientes (nombre, apellido, email, create_at) VALUES ('Fernando', 'Guzman', 'profesor@bolsadeideas.com', '2010-01-01');
