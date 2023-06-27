f7a5cae5116e6cf385ae5fe8b6916c5a6953841f
Mauricio Garcia Garcia <mauriciogarciagarcia37@gmail.com>
2023-06-27 17:48:48 -0600

    Mi tercer commit

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestCotroller.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestCotroller.java
@@ -2,18 +2,26 @@ package com.bolsadeideas.springboot.backend.apirest.controllers;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.services.IClienteService;
+import com.fasterxml.jackson.databind.util.ArrayIterator;
 
+import net.bytebuddy.asm.Advice.Return;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.print.attribute.HashDocAttributeSet;
+import javax.validation.Valid;
 
 import org.apache.coyote.http11.Http11AprProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -61,10 +69,30 @@ public class ClienteRestCotroller {
 	}
 	
 	@PostMapping("/clientes")
-	public ResponseEntity<?> create(@RequestBody Cliente cliente) {
+	public ResponseEntity<?> create(@Valid @RequestBody Cliente cliente, BindingResult result) {
 		
 		Cliente clienteNew = null;
 		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+			
+			/*List<String> errors = new ArrayList<>();
+			
+			for (FieldError err: result.getFieldErrors()) {
+				errors.add("El campo"+ err.getField() + "' " + err.getDefaultMessage());
+				
+			}*/
+			
+			List<String> errors = result.getFieldErrors()
+				.stream()
+				.map(err -> "El campo '"+ err.getField() + "' " + err.getDefaultMessage()
+				)
+				.collect(Collectors.toList());			
+			
+			
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
 
 		try {
 			clienteNew = clienteService.save(cliente);
@@ -82,15 +110,34 @@ public class ClienteRestCotroller {
 	}
 	
 	@PutMapping("/clientes/{id}")
-	public ResponseEntity<?> update(@RequestBody Cliente cliente, @PathVariable Long id) {
+	public ResponseEntity<?> update(@Valid @RequestBody Cliente cliente,BindingResult result, @PathVariable Long id) {
 	
 		Cliente clienteActual = clienteService.findById(id);
 		Cliente clienteUpdated = null;
 
 		Map<String, Object> response = new HashMap<>();
+		if(result.hasErrors()) {
+			
+			/*List<String> errors = new ArrayList<>();
+			
+			for (FieldError err: result.getFieldErrors()) {
+				errors.add("El campo"+ err.getField() + "' " + err.getDefaultMessage());
+				
+			}*/
+			
+			List<String> errors = result.getFieldErrors()
+				.stream()
+				.map(err -> "El campo '"+ err.getField() + "' " + err.getDefaultMessage()
+				)
+				.collect(Collectors.toList());			
+			
+			
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
 		
 		if(clienteActual == null) {
-			response.put("mensaje", "Error: no se pudo editar, el cliente ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
+			response.put("mensaje", "Error: no se pudo editar, el cliente ID: ".concat(id.toString().concat(" no existe en la base de datos")));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
 		} 
 		
@@ -109,7 +156,7 @@ public class ClienteRestCotroller {
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		
-		response.put("mensaje", "El cliente ha sido actulizado con exito");
+		response.put("mensaje", "El cliente ha sido actualizado con exito");
 		response.put("cliente", clienteUpdated);
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED );
 	}
